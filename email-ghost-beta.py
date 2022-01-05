85973c48a813f9d620e6cd5c15fec4c9a9a2eae8
viuy <89278785+viuy@users.noreply.github.com>
2022-01-05 14:46:39 +0100

    Update email-ghost-beta.py

--- email-ghost-beta.py
+++ email-ghost-beta.py
@@ -1,36 +1,37 @@
 import smtplib
 import os
 import sys
-suma = 1
+suma = 0
 numero = 1
-class color:
+
+class bcolors:
     GREEN = "\033[92m"
     RESET = "\033[0m"
     yellow = "\033[93m"
     cyan = "\033[36m"
 
 print(" ")
-print(color.yellow + "EMAIL-GHOST")
+print(bcolors.yellow + "EMAIL-GHOST")
 print(" ")
-print(color.yellow + "by VIUY" )
-print(color.cyan + "TikTok: viuy.tt" )
+print(bcolors.yellow + "by VIUY" )
+print(bcolors.cyan + "TikTok: viuy.tt" )
 print("instagram: viuy_ig")
 print(" ")
-print(color.GREEN + "bienvenido,para usar este script entra a:")
+print(bcolors.GREEN + "bienvenido,para usar este script entra a:")
 print("https://myaccount.google.com/lesssecureapps y permite el acceso de aplicacioes poco seguras")
 print("(con la cuenta con la que vayas a spamear).")
 print(" ")
 
-tucorreo = input("¿con que correo quieres espamear?" + color.RESET + "")
-contraseña = input(color.GREEN + "¿cual es la contraseña del correo?" + color.RESET + "")
-correo = input(color.GREEN + "¿a que correo quieres espamear?" + color.RESET + "")
-mensaje = input(color.GREEN + "¿que mensaje quieres espamear?" + color.RESET + "")
+tucorreo = input("¿con que correo quieres spamear?" + bcolors.RESET + "")
+contraseña = input(bcolors.GREEN + "¿cual es la contraseña del correo?" + bcolors.RESET + "")
+correo = input(bcolors.GREEN + "¿a que correo quieres espamear?" + bcolors.RESET + "")
+mensaje = input(bcolors.GREEN + "¿que mensaje quieres spamear?" + bcolors.RESET + "")
 
 try:
-    veces = int(input(color.GREEN + "¿Cuantas veces?" + color.RESET + ""))
+    veces = int(input(bcolors.GREEN + "¿Cuantas veces?" + bcolors.RESET + ""))
 except ValueError:
     print(" ")
-    print(color.yellow + "ingrese un numero, no puede contener letras" + color.RESET + "")
+    print(bcolors.yellow + "ingrese un numero, no puede contener letras" + bcolors.RESET + "")
     print(" ")
     sys.exit()
 
@@ -48,19 +49,23 @@ def email():
 
         server.quit()
 
-        print(color.GREEN + 'Correo enviado ' + str(suma) + color.RESET + "")
+        print(bcolors.GREEN + 'Correo enviado ' + str(suma) + bcolors.RESET + "")
     
     except smtplib.SMTPAuthenticationError:
         print(" ")
-        print(color.yellow + "tu usuario o contraseña es incorrecto, prueba a cambiarlos" + color.RESET + "")
+        print(bcolors.yellow + "tu usuario o contraseña es incorrecto, prueba a cambiarlos" + bcolors.RESET + "")
         print(" ")         
         sys.exit()
     
     except smtplib.SMTPRecipientsRefused:
         print(" ")
-        print(color.yellow + correo + " no es un correo valido, prueba a cambiarlo" + color.RESET + "")
+        print(bcolors.yellow + correo + " no es un correo valido, prueba a cambiarlo" + bcolors.RESET + "")
         print(" ")
         sys.exit()
+
+if(veces == suma):
+    print(" ")
+    print(bcolors.yellow + "spam terminado correctamente")
 
 for _ in range(veces):
     email()
